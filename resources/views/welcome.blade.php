a7771aed931108e94408e487cc76cb968f8f9393
shabby21321 <shabbiraliasgher9@gmail.com>
2024-05-30 17:39:47 +0500

    Initial commit with Laravel project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+       
+     
+    </head>
+<body class="">
+ 
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid flex justify-content:space-between;" >
+    <a class="navbar-brand" href="#">My Profile</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent" style="
+    padding-left: 75%;
+">
+
+   
+      <form class="d-flex" role="search">
+        <input class="search form-control me-2 flex" type="search" placeholder="Search Task"aria-label="Search" style="
+    border-radius: 50px;
+">
+              </form>
+              <div class="" style="border-radius:50%;background-color:red;">+</div>
+    </div>
+    <div style="border-radius:50%;"><img src="/public/beautiful-girl-with-blue-hair-avatar-of-woman-for-social-network-vector.jpg" alt=""></div>
+
+  </div>
+</nav>
+<div class="container">
+    <div class="" style="margin-top:50px;">
+    <img src="" alt="">
+        <h1 cla>Kai Nani<span style="font-size:15px;padding-left=20px;
+    ">she/her/hers</span></h1>
+        <p style="font:bold"><b>product coordinator for integrations</b></p>
+        <div style="display:flex;">
+            <p style="margin-right:15px">san franisco</p>
+            <p style="margin-right:15px">2:25pm</p>
+            <p style="margin-right:15px">emliabecker@acme.com</p>
+        </div>
+
+    </div>
+    <div style="padding-left: 75%; margin-bottom:30px;">
+        <button class="btn btn-success">Set out of office</button>
+        <button class="btn btn-primary">Edit profile</button>
+    </div>
+<div style="display:flex; justify-content:space-between;">
+    <div class="card" style="width: 51rem;height: 30rem;">
+  <div class="card-body">
+    <h5 class="card-title" style="margin-bottom:35px;"><b>My Projects</b></h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary" style="
+    padding-bottom: 15px;
+"><b> onboarding - BD Engineer</b> <span style="font-size:15px">Private</span></h6>
+    <h6 class="card-subtitle mb-2 text-body-secondary" style="
+    padding-bottom: 15px;
+"><b> onboarding - BD Engineer</b> <span style="font-size:15px">Private</span></h6>
+    <h6 class="card-subtitle mb-2 text-body-secondary" style="
+    padding-bottom: 15px;
+"><b> onboarding - BD Engineer</b> <span style="font-size:15px" >Private</span></h6>
+    <h6 class="card-subtitle mb-2 text-body-secondary" style="
+    padding-bottom: 15px;
+"><b> onboarding - BD Engineer</b> <span style="font-size:15px">Private</span></h6>
+    <h6 class="card-subtitle mb-2 text-body-secondary" style="
+    padding-bottom: 15px;
+"><b> onboarding - BD Engineer</b> <span style="font-size:15px">Private</span></h6>
+    <h6 class="card-subtitle mb-2 text-body-secondary" style="
+    padding-bottom: 15px;
+"><b> onboarding - BD Engineer</b> <span style="font-size:15px">Private</span></h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+   
+  </div>
+</div>
+<div style="display:flex; flex-direction:column;">
+  <div class="card" style="width: 18rem; height:20rem;margin-bottom:30px;">
+  <div class="card-body">
+    <h2 class="card-title" style="padding-bottom:15px;">About Kai</h2>
+   
+    <p class="card-text" style="font-size:15px">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+   
+  </div>
+</div>
+<div class="card" style="width: 18rem; height:20rem">
+  <div class="card-body">
+    <h2 class="card-title" style="padding-bottom:15px;">My teams</h2>
+   
+   <ul style="text-decoration:none;">
+    <li>product <span style="font-size:10px">marketing</span></li>
+    <li>luis <span style="font-size:10px">marketing</span></li>
+    <li>Acme all-staff <span style="font-size:10px">marketing</span></li>
+    <li>IT support <span style="font-size:10px">marketing</span></li>
+    <li>Digital Properties Marketing <span style="font-size:10px">marketing</span></li>
+  <span style="font-size:10px">show more</span>
+   </ul>
+   
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+ 
+
+
+<script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</script>
+    </body>
+</html>
